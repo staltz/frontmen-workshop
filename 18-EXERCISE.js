beac39e2c11917009aedd546bf63129e75a0ad58
Andre Staltz <andre@staltz.com>
2018-01-25 13:31:20 +0100

    Improve exercise 18

--- 18-EXERCISE.js
+++ 18-EXERCISE.js
@@ -6,8 +6,17 @@ function randomSS(cb) {
   cb(Math.random());
 }
 
+function intervalSS(cb) {
+  let i = 1;
+  setInterval(() => { cb(i++); }, 500);
+}
+
 /**
  * Exercise: implement add() for setter-setters
  * and use it to create tenPlusRandom.
  * Hint: beware of race conditions!
  */
+
+function add(xSS, ySS) {
+  // return zSS
+}
